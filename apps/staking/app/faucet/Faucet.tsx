451412f0a0187dc620bcf155e11b95458c2b1fa5
Ryan Miller <aerilym@pm.me>
2024-11-08 09:35:39 +1100

    fix: faucet module tablet padding

--- apps/staking/app/faucet/Faucet.tsx
+++ apps/staking/app/faucet/Faucet.tsx
@@ -37,7 +37,7 @@ export function Faucet({ code }: { code?: string }) {
           <h2 className="text-lg font-semibold">{dictionary('walletRequirementTitle')}</h2>
           <p>{dictionary.rich('walletRequirementDescription')}</p>
         </div>
-        <div className="h-max min-h-[400px]">
+        <div className="mt-6 h-max min-h-[400px] w-full max-w-3xl">
           <AuthModule code={code} />
         </div>
       </div>
