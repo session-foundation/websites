c01c926ad4d1750c0353a574f36bdd77f46732fd
Ryan Miller <aerilym@pm.me>
2024-11-28 16:07:06 +1100

    fix: use correct sitemap url

--- apps/foundation/app/robots.ts
+++ apps/foundation/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/studio/',
     },
-    sitemap: 'https://acme.com/sitemap.xml',
+    sitemap: 'https://session.foundation/sitemap.xml',
   };
 }
