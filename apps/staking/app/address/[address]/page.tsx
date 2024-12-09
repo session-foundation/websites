992bc11562b87b55fcbe2e8793cbf2d32fbd7b8a
Ryan Miller <aerilym@pm.me>
2024-12-10 10:28:17 +1100

    fix: update address page with new next page pattern

--- apps/staking/app/address/[address]/page.tsx
+++ apps/staking/app/address/[address]/page.tsx
@@ -1,28 +1,6 @@
-import { StakedNodesWithAddress } from '@/app/mystakes/modules/StakedNodesModule';
-import { isAddress } from 'viem';
-import { notFound } from 'next/navigation';
-import { ModuleGrid } from '@session/ui/components/ModuleGrid';
-import BalanceModule from '@/app/mystakes/modules/BalanceModule';
-import TotalRewardsModule from '@/app/mystakes/modules/TotalRewardsModule';
-import UnclaimedTokensModule from '@/app/mystakes/modules/UnclaimedTokensModule';
+import { AddressInfo } from '@/app/address/[address]/addressInfo';
 
 export default async function Page(props: { params: Promise<{ address: string }> }) {
   const params = await props.params;
-
-  const {
-    address
-  } = params;
-
-  return isAddress(address) ? (
-    <div className="flex flex-col gap-4">
-      <ModuleGrid>
-        <BalanceModule addressOverride={address} />
-        <TotalRewardsModule addressOverride={address} />
-        <UnclaimedTokensModule addressOverride={address} />
-      </ModuleGrid>
-      <StakedNodesWithAddress address={address} />
-    </div>
-  ) : (
-    notFound()
-  );
+  return <AddressInfo address={params.address} />;
 }
--- apps/staking/app/address/page.tsx
+++ apps/staking/app/address/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import AddressPage from '@/app/address/[address]/page';
 import { useWallet } from '@session/wallet/hooks/wallet-hooks';
+import { AddressInfo } from '@/app/address/[address]/addressInfo';
 
 export default function Page() {
   const { address } = useWallet();
-  return address ? <AddressPage params={{ address }} /> : null;
+  return address ? <AddressInfo address={address} /> : null;
 }
