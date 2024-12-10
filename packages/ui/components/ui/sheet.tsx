9729393f118038fb7fd3186dcc3039e80f40a850
Ryan Miller <aerilym@pm.me>
2024-12-11 09:27:12 +1100

    chore: remove unused code

--- packages/ui/components/ui/sheet.tsx
+++ packages/ui/components/ui/sheet.tsx
@@ -91,27 +91,6 @@ const SheetContent = React.forwardRef<
 ));
 SheetContent.displayName = SheetPrimitive.Content.displayName;
 
-//
-// const SheetContent = React.forwardRef<
-//   React.ElementRef<typeof SheetPrimitive.Content>,
-//   SheetContentProps
-// >(({ side = 'right', className, children, closeSheet, ...props }, ref) => (
-//   <SheetPortal>
-//     <SheetOverlay />
-//     <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
-//       {children}
-//       <SheetPrimitive.Close
-//         className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none"
-//         onClick={closeSheet}
-//       >
-//         <X className="h-4 w-4" />
-//         <span className="sr-only">Close</span>
-//       </SheetPrimitive.Close>
-//     </SheetPrimitive.Content>
-//   </SheetPortal>
-// ));
-// SheetContent.displayName = SheetPrimitive.Content.displayName;
-
 const SheetHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
   <div className={cn('flex flex-col space-y-2 text-center sm:text-left', className)} {...props} />
 );
