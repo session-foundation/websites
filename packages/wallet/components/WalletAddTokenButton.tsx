e242a47fe8880c6499f0ca223494a9dc8ca6d7d9
Ryan Miller <aerilym@pm.me>
2024-12-10 14:44:52 +1100

    feat: replace web3modal with web3sheet and refactor hooks and components

--- packages/wallet/components/WalletAddTokenButton.tsx
+++ packages/wallet/components/WalletAddTokenButton.tsx
@@ -1,10 +1,4 @@
-import { CHAIN } from '@session/contracts/chains';
-import { Button, type ButtonVariantProps } from '@session/ui/components/ui/button';
-import { useWeb3Modal } from '@web3modal/wagmi/react';
-import { useMemo } from 'react';
-import { useAddSessionTokenToWallet, useWalletChain } from '../hooks/wallet-hooks';
-import { ButtonDataTestId } from '../testing/data-test-ids';
-import { toast } from '@session/ui/lib/toast';
+import { type ButtonVariantProps } from '@session/ui/components/ui/button';
 
 export type WalletAddTokenButtonProps = ButtonVariantProps & {
   tokenIcon: string;
@@ -24,68 +18,69 @@ export type WalletAddTokenButtonProps = ButtonVariantProps & {
 };
 
 export default function WalletAddTokenButton(props: WalletAddTokenButtonProps) {
-  const { addToken, error, isPending } = useAddSessionTokenToWallet(props.tokenIcon);
-  const { chain } = useWalletChain();
-  const { open } = useWeb3Modal();
-
-  const onCorrectChain = chain === CHAIN.MAINNET || chain === CHAIN.TESTNET;
-
-  const handleClick = () => {
-    if (!onCorrectChain) {
-      open({ view: 'Networks' });
-    } else {
-      addToken();
-    }
-  };
-
-  if (error) {
-    console.error(error);
-    toast.error(props.errors.fail);
-  }
-
-  return (
-    <AddTokenButton
-      {...props}
-      handleClick={handleClick}
-      onCorrectChain={onCorrectChain}
-      isPending={isPending}
-    />
-  );
-}
-
-type AddTokenButtonProps = WalletAddTokenButtonProps & {
-  handleClick: () => void;
-  onCorrectChain: boolean;
-  isPending: boolean;
-};
-
-export function AddTokenButton({
-  labels,
-  ariaLabels,
-  handleClick,
-  isPending,
-  onCorrectChain,
-  ...props
-}: AddTokenButtonProps) {
-  const [label, ariaLabel] = useMemo(() => {
-    if (isPending) {
-      return [labels.pending, ariaLabels.pending];
-    }
-    return onCorrectChain
-      ? [labels.addToken, ariaLabels.addToken]
-      : [labels.changeNetwork, ariaLabels.changeNetwork];
-  }, [isPending, onCorrectChain, labels, ariaLabels]);
-
-  return (
-    <Button
-      onClick={handleClick}
-      disabled={isPending}
-      data-testid={ButtonDataTestId.Add_Token}
-      className="gap-1 px-2 py-1 uppercase"
-      aria-label={ariaLabel}
-      {...props}
-    >
-      {label}
-    </Button>
-  );
+  return null;
+  //   const { addToken, error, isPending } = useAddSessionTokenToWallet(props.tokenIcon);
+  //   const { chain } = useWalletChain();
+  //   const { open } = useWeb3Modal();
+  //
+  //   const onCorrectChain = chain === CHAIN.MAINNET || chain === CHAIN.TESTNET;
+  //
+  //   const handleClick = () => {
+  //     if (!onCorrectChain) {
+  //       open({ view: 'Networks' });
+  //     } else {
+  //       addToken();
+  //     }
+  //   };
+  //
+  //   if (error) {
+  //     console.error(error);
+  //     toast.error(props.errors.fail);
+  //   }
+  //
+  //   return (
+  //     <AddTokenButton
+  //       {...props}
+  //       handleClick={handleClick}
+  //       onCorrectChain={onCorrectChain}
+  //       isPending={isPending}
+  //     />
+  //   );
+  // }
+  //
+  // type AddTokenButtonProps = WalletAddTokenButtonProps & {
+  //   handleClick: () => void;
+  //   onCorrectChain: boolean;
+  //   isPending: boolean;
+  // };
+  //
+  // export function AddTokenButton({
+  //   labels,
+  //   ariaLabels,
+  //   handleClick,
+  //   isPending,
+  //   onCorrectChain,
+  //   ...props
+  // }: AddTokenButtonProps) {
+  //   const [label, ariaLabel] = useMemo(() => {
+  //     if (isPending) {
+  //       return [labels.pending, ariaLabels.pending];
+  //     }
+  //     return onCorrectChain
+  //       ? [labels.addToken, ariaLabels.addToken]
+  //       : [labels.changeNetwork, ariaLabels.changeNetwork];
+  //   }, [isPending, onCorrectChain, labels, ariaLabels]);
+  //
+  //   return (
+  //     <Button
+  //       onClick={handleClick}
+  //       disabled={isPending}
+  //       data-testid={ButtonDataTestId.Add_Token}
+  //       className="gap-1 px-2 py-1 uppercase"
+  //       aria-label={ariaLabel}
+  //       {...props}
+  //     >
+  //       {label}
+  //     </Button>
+  //   );
 }
---
+++ packages/wallet/providers/web3wallet-provider.tsx
@@ -0,0 +1,196 @@
+'use client';
+
+import { forwardRef, type ReactNode, useState } from 'react';
+import { createWeb3WalletConfig, QueryProvider, WalletProvider } from '@web3sheet/core';
+import { arbitrum, arbitrumSepolia, mainnet, sepolia } from 'viem/chains';
+import { Button, type ButtonProps } from '@session/ui/ui/button';
+import { ArrowDownIcon } from '@session/ui/icons/ArrowDownIcon';
+import { Avatar, AvatarFallback, AvatarImage } from '@session/ui/ui/avatar';
+import { cn } from '@session/ui/lib/utils';
+import type { DynamicTokenRowProps } from '@web3sheet/core/hooks/useWallet';
+import type { Web3WalletComponentLibrary } from '@web3sheet/ui/lib/library';
+import { Switch } from '@session/ui/ui/switch';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetOverlay,
+  SheetTitle,
+} from '@session/ui/ui/sheet';
+
+const TabFullWidthButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, ...props }, ref) => (
+    <Button {...props} className={cn('w-full', className)} variant="outline" ref={ref}>
+      {children}
+    </Button>
+  )
+);
+
+const ButtonWithIconSrc = forwardRef<HTMLButtonElement, ButtonProps & { src: string }>(
+  ({ src, children, className, ...props }, ref) => (
+    <TabFullWidthButton {...props} ref={ref} className={cn('relative', className)}>
+      <div className="absolute h-6 w-6" style={{ left: '6px' }}>
+        <Avatar className="h-6 w-6">
+          <AvatarImage src={src} />
+          <AvatarFallback className="bg-black" />
+        </Avatar>
+      </div>
+      {children}
+    </TabFullWidthButton>
+  )
+);
+
+const ButtonWithIconReactNode = forwardRef<HTMLButtonElement, ButtonProps & { icon: ReactNode }>(
+  ({ icon, children, className, ...props }, ref) => (
+    <TabFullWidthButton {...props} ref={ref} className={cn('relative', className)}>
+      <div className="absolute h-6 w-6" style={{ left: '6px' }}>
+        {icon}
+      </div>
+      {children}
+    </TabFullWidthButton>
+  )
+);
+
+const BackButton = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => (
+  <Button {...props} ref={ref} variant="ghost" size="icon">
+    <ArrowDownIcon className="fill-session-text mt-0.5 h-3 w-3 rotate-90" />
+  </Button>
+));
+
+const componentLibrary: Web3WalletComponentLibrary = {
+  // @ts-expect-error -- TODO: deal with data test ids
+  TabFullWidthButton,
+  // @ts-expect-error -- TODO: deal with data test ids
+  ButtonWithIconReactNode,
+  // @ts-expect-error -- TODO: deal with data test ids
+  ButtonWithIconSrc,
+  // @ts-expect-error -- TODO: deal with data test ids
+  BackButton,
+  Switch,
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetOverlay,
+};
+
+function TokenActionButton({ href, children }: { href: string; children: ReactNode }) {
+  return (
+    <a href={href}>
+      <Button size="xs" variant="outline" data-testid="button:token-action">
+        {children}
+      </Button>
+    </a>
+  );
+}
+
+const tokenDetailsArbitrum: DynamicTokenRowProps = {
+  tokenAddress: '0x70c1f36C9cEBCa51B9344121D284D85BE36CD6bB',
+  name: 'Session Token',
+  iconSrc: '/images/token_logo.svg',
+  network: {
+    id: arbitrum.id,
+    name: 'Arbitrum One',
+    iconSrc: '/images/arbitrum.svg',
+  },
+  children: <TokenActionButton href="/stake">Stake</TokenActionButton>,
+};
+
+const tokenDetailsArbitrumSepolia: DynamicTokenRowProps = {
+  tokenAddress: '0x70c1f36C9cEBCa51B9344121D284D85BE36CD6bB',
+  name: 'Session Token',
+  iconSrc: '/images/token_logo.svg',
+  symbolPrefix: 't',
+  network: {
+    id: arbitrumSepolia.id,
+    name: 'Arbitrum Sepolia',
+    iconSrc: '/images/arbitrum.svg',
+  },
+  children: <TokenActionButton href="/stake">Stake</TokenActionButton>,
+};
+
+const tokenDetailsEthereum: DynamicTokenRowProps = {
+  tokenAddress: '0x70c1f36C9cEBCa51B9344121D284D85BE36CD6bB',
+  name: 'Session Token',
+  iconSrc: '/images/token_logo.svg',
+  network: {
+    id: mainnet.id,
+    name: 'Ethereum',
+    iconSrc: '/images/eth.svg',
+    className: 'bg-session-white',
+  },
+  children: <TokenActionButton href="/bridge">Bridge</TokenActionButton>,
+};
+
+const tokenDetailsEthereumSepolia: DynamicTokenRowProps = {
+  tokenAddress: '0x70c1f36C9cEBCa51B9344121D284D85BE36CD6bB',
+  name: 'Session Token',
+  iconSrc: '/images/token_logo.svg',
+  symbolPrefix: 't',
+  network: {
+    id: sepolia.id,
+    name: 'Ethereum Sepolia',
+    iconSrc: '/images/eth.svg',
+    className: 'bg-session-white',
+  },
+  children: <TokenActionButton href="/bridge">Bridge</TokenActionButton>,
+};
+
+const tokenDetailsWOXENEthereum: DynamicTokenRowProps = {
+  tokenAddress: '0xd1e2d5085b39b80c9948aeb1b9aa83af6756bcc5',
+  name: 'Wrapped OXEN',
+  iconSrc: '/images/woxen.svg',
+  network: {
+    id: mainnet.id,
+    name: 'Ethereum',
+    iconSrc: '/images/eth.svg',
+    className: 'bg-session-white',
+  },
+  children: <TokenActionButton href="https://ethereum.oxen.io">Swap</TokenActionButton>,
+};
+
+const createConfig = (projectId: string) => {
+  const config = createWeb3WalletConfig({
+    wagmiConfig: {
+      chains: [arbitrum, arbitrumSepolia, mainnet, sepolia],
+    },
+    walletConnectConfig: {
+      projectId,
+    },
+    metaMaskConfig: {
+      dappMetadata: {
+        name: 'Session Wallet',
+        url: 'https://stake.getsession.org',
+      },
+    },
+  });
+
+  config.componentLibrary = componentLibrary;
+  config.tokens = [
+    tokenDetailsArbitrum,
+    tokenDetailsEthereum,
+    tokenDetailsArbitrumSepolia,
+    tokenDetailsEthereumSepolia,
+    tokenDetailsWOXENEthereum,
+  ];
+
+  return config;
+};
+
+export type Web3WalletProviderProps = {
+  children: ReactNode;
+  projectId: string;
+  wagmiCookie?: string | undefined | null;
+};
+
+export function Web3WalletProvider({ children, projectId, wagmiCookie }: Web3WalletProviderProps) {
+  const [config] = useState(createConfig(projectId));
+
+  return (
+    <WalletProvider config={config} wagmiCookie={wagmiCookie}>
+      <QueryProvider>{children}</QueryProvider>
+    </WalletProvider>
+  );
+}
