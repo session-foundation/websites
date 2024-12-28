2c711a339a482d0aeb65458511180f35919a7e1c
Ryan Miller <aerilym@pm.me>
2024-12-29 09:37:08 +1100

    feat: create stake state parser

---
+++ apps/staking/components/StakedNode/state.ts
@@ -0,0 +1,117 @@
+import {
+  CONTRIBUTION_CONTRACT_STATUS,
+  type ContributorContractInfo,
+  type Stake,
+} from '@session/staking-api-js/client';
+
+enum EVENT {
+  NewServiceNodeV2 = 'NewServiceNodeV2',
+  ServiceNodeExitRequest = 'ServiceNodeExitRequest',
+  ServiceNodeExit = 'ServiceNodeExit',
+  ServiceNodeLiquidated = 'ServiceNodeLiquidated',
+}
+
+enum STAKE_EVENT_STATE {
+  UNKNOWN,
+  ACTIVE,
+  EXIT_REQUESTED,
+  EXITED,
+  LIQUIDATED,
+}
+
+export function parseStakeEventState(stake: Stake) {
+  const latestEvent = stake.events[0];
+
+  if (!latestEvent) return STAKE_EVENT_STATE.UNKNOWN;
+
+  switch (latestEvent.name) {
+    case EVENT.NewServiceNodeV2:
+      return STAKE_EVENT_STATE.ACTIVE;
+    case EVENT.ServiceNodeExitRequest:
+      return STAKE_EVENT_STATE.EXIT_REQUESTED;
+    case EVENT.ServiceNodeExit:
+      return STAKE_EVENT_STATE.EXITED;
+    case EVENT.ServiceNodeLiquidated:
+      return STAKE_EVENT_STATE.LIQUIDATED;
+    default:
+      return STAKE_EVENT_STATE.UNKNOWN;
+  }
+}
+
+export enum STAKE_STATE {
+  /** @see {STAKE_EVENT_STATE.EXITED}
+   * OR
+   * @see {STAKE_EVENT_STATE.LIQUIDATED}
+   *
+   * To determine if the stake was exited by `unlock` or `deregistration`, use {@link isStakeDeregistered}
+   * */
+  EXITED = 'Exited',
+  /** @see {STAKE_EVENT_STATE.EXIT_REQUESTED} */
+  AWAITING_EXIT = 'Awaiting Exit',
+  /**
+   * If a stake is in the `deregistered` state, it means that the node has been deregistered by the network. All states are mutually exclusive, so it can't also be {@link STAKE_STATE.EXITED}.
+   */
+  DEREGISTERED = 'Deregistered',
+  /** @see {STAKE_EVENT_STATE.ACTIVE}
+   * and`active = true */
+  RUNNING = 'Running',
+  /** @see {STAKE_EVENT_STATE.ACTIVE}
+   * and`active = false */
+  DECOMMISSIONED = 'Decommissioned',
+  /** Unknown state */
+  UNKNOWN = 'Unknown',
+}
+
+export function isStakeDeregistered(stake: Stake) {
+  return !!(
+    stake.exit_type === 'deregistration' &&
+    stake.deregistration_height &&
+    stake.deregistration_height > 0
+  );
+}
+
+export function parseStakeState(stake: Stake) {
+  const eventState = parseStakeEventState(stake);
+
+  if (eventState === STAKE_EVENT_STATE.EXITED || eventState === STAKE_EVENT_STATE.LIQUIDATED) {
+    return STAKE_STATE.EXITED;
+  }
+
+  if (isStakeDeregistered(stake)) {
+    return STAKE_STATE.DEREGISTERED;
+  }
+
+  if (eventState === STAKE_EVENT_STATE.EXIT_REQUESTED) {
+    return STAKE_STATE.AWAITING_EXIT;
+  }
+
+  if (eventState === STAKE_EVENT_STATE.ACTIVE) {
+    return stake.active ? STAKE_STATE.RUNNING : STAKE_STATE.DECOMMISSIONED;
+  }
+
+  return STAKE_STATE.UNKNOWN;
+}
+
+export enum STAKE_CONTRACT_STATE {
+  AWAITING_OPERATOR_START = 'Awaiting Operator Start',
+  // NOTE: only show a joining contract if there is no related stake from the stakes list
+  JOINING = 'Joining',
+  AWAITING_CONTRIBUTORS = 'Awaiting Contributors',
+  AWAITING_OPERATOR_CONTRIBUTION = 'Awaiting Operator Contribution',
+  UNKNOWN = 'Unknown',
+}
+
+export function parseStakeContractState(contract: ContributorContractInfo) {
+  switch (contract.status) {
+    case CONTRIBUTION_CONTRACT_STATUS.WaitForOperatorContrib:
+      return STAKE_CONTRACT_STATE.AWAITING_OPERATOR_CONTRIBUTION;
+    case CONTRIBUTION_CONTRACT_STATUS.OpenForPublicContrib:
+      return STAKE_CONTRACT_STATE.AWAITING_CONTRIBUTORS;
+    case CONTRIBUTION_CONTRACT_STATUS.WaitForFinalized:
+      return STAKE_CONTRACT_STATE.AWAITING_OPERATOR_START;
+    case CONTRIBUTION_CONTRACT_STATUS.Finalized:
+      return STAKE_CONTRACT_STATE.JOINING;
+    default:
+      return STAKE_CONTRACT_STATE.UNKNOWN;
+  }
+}
