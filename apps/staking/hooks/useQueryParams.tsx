bda67e3fb61804d844fa8a961f58b1cdaa3e020f
Ryan Miller <aerilym@pm.me>
2025-01-09 10:18:29 +1100

    feat: create useQueryParams hook

---
+++ apps/staking/hooks/useQueryParams.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
+
+type QueryParamValue = string | number | boolean;
+
+export type UseQueryParamsReturn<K = string> = {
+  getQueryParams: () => URLSearchParams;
+  pushQueryParam: (name: K, value: QueryParamValue) => void;
+};
+
+export default function useQueryParams<K = string>(): UseQueryParamsReturn<K> {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const getQueryParams = useCallback(() => {
+    return new URLSearchParams(searchParams.toString());
+  }, [searchParams]);
+
+  const createQueryString = useCallback(
+    (name: K, value: QueryParamValue) => {
+      const params = getQueryParams();
+      params.set(name as string, String(value));
+
+      return params.toString();
+    },
+    [getQueryParams]
+  );
+
+  const pushQueryParam = useCallback(
+    (name: K, value: QueryParamValue) => {
+      const str = `${pathname}?${createQueryString(name, value)}`;
+      router.push(str);
+    },
+    [pathname, createQueryString]
+  );
+
+  return { getQueryParams, pushQueryParam };
+}
