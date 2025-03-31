fe09a5f120a854214ae9ca704f264dafa1329a4b
Ryan Miller <aerilym@pm.me>
2025-03-31 11:01:06 +1100

    chore: create contract parsing tests

--- apps/staking/app/register/[nodeId]/NodeRegistration.tsx
+++ apps/staking/app/register/[nodeId]/NodeRegistration.tsx
@@ -7,13 +7,33 @@ import { useCallback, useEffect, useMemo } from 'react';
 
 import { NodeRegistrationFormSkeleton } from '@/app/register/[nodeId]/NodeRegistrationFormSkeleton';
 import { useRegistrationsForCurrentActor } from '@/hooks/useRegistrationsForCurrentActor';
+import logger from '@/lib/logger';
+import { getNodeRegistrationsForSnKey } from '@/lib/queries/getNodeRegistrationsForSnKey';
 import { useStakingBackendBrowserClient } from '@/lib/staking-api-client';
+import { useVesting } from '@/providers/vesting-provider';
+import { isEd25519PublicKey } from '@session/staking-api-js/refine';
+import { safeTrySync, safeTrySyncWithFallback } from '@session/util-js/try';
+import { useWallet } from '@session/wallet/hooks/useWallet';
 import { useTranslations } from 'next-intl';
+import { usePathname } from 'next/navigation';
 
 export default function NodeRegistration({ nodeId }: { nodeId: string }) {
+  const pathname = usePathname();
+  const stakingBackendClient = useStakingBackendBrowserClient();
+
+  const { address: walletAddress } = useWallet();
 
   const { data: registrationsData, isLoading: isLoadingRegistrations } =
     useRegistrationsForCurrentActor();
+
+  const {
+    connectToVestingContract,
+    disconnectFromVestingContract,
+    activeContract,
+    contracts,
+    setShowVestingSelectionDialog,
+  } = useVesting();
+
   const dict = useTranslations('actionModules.registration.shared');
 
   const { isLoading: isLoadingStakes } = useStakes();
---
+++ apps/staking/tests/testUtils.ts
@@ -0,0 +1,68 @@
+import { ARBITRUM_EVENT } from '@session/staking-api-js/enums';
+import type { Ed25519PublicKey } from '@session/staking-api-js/refine';
+import type {
+  ArbitrumEvent,
+  ContributionContractContributor,
+} from '@session/staking-api-js/schema';
+import type { Address } from 'viem';
+
+const WALLET_ADDR_START = `0x${'0'.repeat(37)}a`;
+const CONTRACT_ADDR_START = `0x${'0'.repeat(37)}c`;
+const ED25519_ADDR_START = '0'.repeat(62);
+
+export const WALLET_ADDRESS = {
+  1: `${WALLET_ADDR_START}0a` as Address,
+  2: `${WALLET_ADDR_START}0b` as Address,
+  3: `${WALLET_ADDR_START}0c` as Address,
+  4: `${WALLET_ADDR_START}0d` as Address,
+  5: `${WALLET_ADDR_START}0e` as Address,
+  6: `${WALLET_ADDR_START}0f` as Address,
+  7: `${WALLET_ADDR_START}10` as Address,
+  8: `${WALLET_ADDR_START}11` as Address,
+  9: `${WALLET_ADDR_START}12` as Address,
+};
+
+export const CONTRACT_ADDRESS = {
+  1: `${CONTRACT_ADDR_START}0a` as Address,
+  2: `${CONTRACT_ADDR_START}0b` as Address,
+  3: `${CONTRACT_ADDR_START}0c` as Address,
+  4: `${CONTRACT_ADDR_START}0d` as Address,
+  5: `${CONTRACT_ADDR_START}0e` as Address,
+  6: `${CONTRACT_ADDR_START}0f` as Address,
+  7: `${CONTRACT_ADDR_START}10` as Address,
+  8: `${CONTRACT_ADDR_START}11` as Address,
+  9: `${CONTRACT_ADDR_START}12` as Address,
+};
+
+export const ED25519_ADDRESS = {
+  1: `${ED25519_ADDR_START}0a` as Ed25519PublicKey,
+  2: `${ED25519_ADDR_START}0b` as Ed25519PublicKey,
+  3: `${ED25519_ADDR_START}0c` as Ed25519PublicKey,
+  4: `${ED25519_ADDR_START}0d` as Ed25519PublicKey,
+  5: `${ED25519_ADDR_START}0e` as Ed25519PublicKey,
+  6: `${ED25519_ADDR_START}0f` as Ed25519PublicKey,
+  7: `${ED25519_ADDR_START}10` as Ed25519PublicKey,
+  8: `${ED25519_ADDR_START}11` as Ed25519PublicKey,
+  9: `${ED25519_ADDR_START}12` as Ed25519PublicKey,
+};
+
+export const DEPLOY_ARB_EVENT = (block: number) => {
+  return {
+    name: ARBITRUM_EVENT.NewServiceNodeContributionContract,
+    block,
+  } as ArbitrumEvent;
+};
+export const FINALIZED_ARB_EVENT = (block: number) => {
+  return {
+    name: ARBITRUM_EVENT.Finalized,
+    block,
+  } as ArbitrumEvent;
+};
+export const CONTRIBUTOR = (address: Address, amount = 0n, reserved = 0n) => {
+  return {
+    address,
+    beneficiary_address: address,
+    amount,
+    reserved,
+  } as ContributionContractContributor;
+};
