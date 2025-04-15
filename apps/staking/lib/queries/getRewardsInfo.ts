4836a54dcae04e51c93fe50ad7d046849f431d2d
Ryan Miller <aerilym@pm.me>
2025-04-15 10:37:48 +1000

    feat: add basic v2 rewards user preference switching for api calls

--- apps/staking/lib/queries/getRewardsInfo.ts
+++ apps/staking/lib/queries/getRewardsInfo.ts
@@ -1,17 +1,27 @@
 import type { SessionStakingClient } from '@session/staking-api-js/client';
-import { blsRewardsResponseSchema } from '@session/staking-api-js/schema';
+import {
+  blsRewardsResponseSchema,
+  v1BlsRewardsResponseSchema,
+} from '@session/staking-api-js/schema';
 import type { Address } from 'viem';
 
 /**
  * Retrieves the rewards info for a given address.
  * @param client The staking backend client.
  * @param address The address of the node.
+ * @param v2 Whether to use the v2 rewards endpoint.
  * @returns The rewards info for the node.
  */
 export const getRewardsInfo = async (
   client: SessionStakingClient,
-  { address }: { address: Address }
+  { address, v2 = true }: { address: Address; v2?: boolean }
 ) => {
+  if (!v2) {
+    const res = await client.getRewardsInfoV1({ address });
+    res.data = v1BlsRewardsResponseSchema.parse(res.data);
+    return res;
+  }
+
   const res = await client.getRewardsInfo({ address });
   res.data = blsRewardsResponseSchema.parse(res.data);
   return res;
